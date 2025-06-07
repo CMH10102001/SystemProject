dae1e6f03f54b398a5dff51e4ae058655d190481
CMH10102001 <140772597+CMH10102001@users.noreply.github.com>
2025-06-07 16:25:56 +0800

    Add files via upload

---
+++ Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Smile___Sunshine_Toy_Co__Ltd_CCMS
+{
+    public partial class Task : Form
+    {
+        public Task()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
